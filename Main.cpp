904ca0059bcbc6387cc755b2263a4c1b8d1b0b9f
calvinespinoza <calvin_espinoza@hotmail.com>
2017-03-10 15:14:49 -0600

    Piezas.

--- Main.cpp
+++ Main.cpp
@@ -16,8 +16,122 @@ int tirarDado();
 int main()
 {
 	int size = 15;
+	int opcion1 = 0, opcion2 = 0;
 	char** mat = NULL;
 	mat = inicializarMatriz(size);
+
+	Pieza* pieza1 = new Pieza();
+	Pieza* pieza2 = new Pieza();
+
+	do {
+		cout << "JUGADOR 1" << endl
+		<< "Elegir color de tu pieza: " << endl
+		<< "1. Azul\n2. Rojo\n3. Verde\n4. Amarillo\n";
+		cin >> opcion1;
+	} while(opcion1 < 1 || opcion1 > 4);
+
+	bool ask = false;
+	do {
+		cout << "JUGADOR 2" << endl
+		<< "Elegir color de tu pieza: " << endl;
+		if (opcion1 == 1)
+		{
+			PiezaAzul* pieza1 = new PiezaAzul(14, 14, "Azul");
+			cout << "1. Rojo\n2. Verde\n3. Amarillo\n";
+			cin >> opcion2;
+			if (opcion2 == 1)
+			{
+				PiezaRoja* pieza2 = new PiezaRoja(0, 14, "Roja");
+			}
+			else if (opcion2 == 2)
+			{
+				PiezaVerde* pieza2 = new PiezaVerde(0, 0, "Verde");
+			}
+			else if (opcion2 == 3)
+			{
+				PiezaAmarilla* pieza2 = new PiezaAmarilla(14, 0, "Amarillo");
+			}
+			else
+			{
+				ask = true;
+			}
+
+		}
+		else if (opcion1 == 2)
+		{
+			PiezaRoja* pieza1 = new PiezaRoja(0, 14, "Roja");
+			cout << "1. Azul\n2. Verde\n3. Amarillo\n";
+			cin >> opcion2;
+			if (opcion2 == 1)
+			{
+				PiezaAzul* pieza2 = new PiezaAzul(14, 14, "Azul");
+			}
+			else if (opcion2 == 2)
+			{
+				PiezaVerde* pieza2 = new PiezaVerde(0, 0, "Verde");
+			}
+			else if (opcion2 == 3)
+			{
+				PiezaAmarilla* pieza2 = new PiezaAmarilla(14, 0, "Amarillo");
+			}
+			else
+			{
+				ask = true;
+			}
+		}
+		else if (opcion1 == 3)
+		{
+			PiezaVerde* pieza1 = new PiezaVerde(0, 0, "Verde");
+			cout << "1. Azul\n2. Rojo\n3. Amarillo\n";
+			cin >> opcion2;
+			if (opcion2 == 1)
+			{
+				PiezaAzul* pieza2 = new PiezaAzul(14, 14, "Azul");
+			}
+			else if (opcion2 == 2)
+			{
+				PiezaRoja* pieza2 = new PiezaRoja(0, 14, "Roja");
+			}
+			else if (opcion2 == 3)
+			{
+				PiezaAmarilla* pieza2 = new PiezaAmarilla(14, 0, "Amarillo");
+			}
+			else
+			{
+				ask = true;
+			}
+		} else if (opcion1 == 4)
+		{
+			PiezaAmarilla* pieza1 = new PiezaAmarilla(14, 0, "Amarillo");
+			cout << "1. Azul\n2. Rojo\n3. Verde\n";
+			cin >> opcion2;
+			if (opcion2 == 1)
+			{
+				PiezaAzul* pieza2 = new PiezaAzul(14, 14, "Azul");
+			}
+			else if (opcion2 == 2)
+			{
+				PiezaRoja* pieza2 = new PiezaRoja(0, 14, "Roja");
+			}
+			else if (opcion2 == 3)
+			{
+				PiezaVerde* pieza2 = new PiezaVerde(0, 0, "Verde");
+			}
+			else
+			{
+				ask = true;
+			}
+		}
+
+	} while(ask);
+
+
+	int one = pieza1 -> getPosX();
+	int two = pieza1 -> getPosY();
+	int three = pieza2 -> getPosX();
+	int four = pieza2 -> getPosY();
+
+	cout << one << endl << two << endl << three << endl << four << endl;
 
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
@@ -33,6 +147,10 @@ int main()
 				mat[i][j] = '/';
 			else if (i > 7 && j == 7)
 				mat[i][j] = '#';
+			else if (i == pieza1 -> getPosX() && j == pieza1 -> getPosY())
+				mat[i][j] = '1';
+			else if (i == pieza2 -> getPosX() && j == pieza2 -> getPosY())
+				mat[i][j] = '2';
 			else
 				mat[i][j] = ' ';
 		}
@@ -68,5 +186,5 @@ char** inicializarMatriz(int size){
 
 int tirarDado()
 {
-	
+	return 0;
 }
