b1afd01d72d4d6b8a32d628a83704cd490ede056
calvinespinoza <calvin_espinoza@hotmail.com>
2017-03-10 13:51:10 -0600

    Trabajando2

---
+++ Pieza.cpp
@@ -0,0 +1,45 @@
+#include "Pieza.h"
+
+Pieza::Pieza()
+{
+	posx = 0;
+	posy = 0;
+	color = "";
+}
+
+Pieza::Pieza(int pPosx, int pPosy, string pColor)
+{
+	posx = pPosx;
+	posy = pPosy;
+	color = pColor;
+}
+
+int Pieza::getPosX()
+{
+	return posx;
+}
+
+int Pieza::getPosY()
+{
+	return posy;
+}
+
+string Pieza::getColor()
+{
+	return color;
+}
+
+void Pieza::setPosX(int pPosx)
+{
+	posx = pPosx;
+}
+
+void Pieza::setPosY(int pPosy)
+{
+	posy = pPosy;
+}
+
+void Pieza::setColor(string pColor)
+{
+	color = pColor;
+}
